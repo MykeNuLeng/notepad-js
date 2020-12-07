d6cbc12fee6833aaa69d50edf3be1b97f0b6c421
Michael Linford <linford.mike@gmail.com>
2020-12-07 15:32:43 +0000

    abstracted assert method from testing framework

---
+++ spec/note-modelSpec.js
@@ -0,0 +1,9 @@
+(function() {
+  function testCreateNote(text) {
+    var note = new Note(text);
+
+    assert.isTrue(note.view === text)
+  }
+
+  testCreateNote("Hello world!");
+})();
---
+++ src/note-model.js
@@ -0,0 +1,5 @@
+class Note {
+  constructor(text) {
+    this.view = 0;
+  };
+}
