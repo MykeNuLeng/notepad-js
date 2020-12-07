0a1d442d1c45617ae5b36405da8c669df76a3336
Michael Linford <linford.mike@gmail.com>
2020-12-07 18:31:14 +0000

    added a NoteListView object that initializes with a NoteList object, with a display() method, that will return a string of the notes in html

---
+++ spec/note-list-viewSpec.js
@@ -0,0 +1,18 @@
+function testNoteListView() {
+  var noteListView = new NoteListView();
+  noteListView.noteList.addNote("Im looking dummy thicc")
+
+  assert.isTrue(noteListView.display() === '<ul><li><div>Im looking dummy thicc</div></li></ul>')
+}
+
+testNoteListView()
+
+function testTwoNotes() {
+  var noteListView = new NoteListView();
+  noteListView.noteList.addNote("Im looking dummy thicc")
+  noteListView.noteList.addNote("Honestly, wheres the lie")
+
+  assert.isTrue(noteListView.display() === '<ul><li><div>Im looking dummy thicc</div></li><li><div>Honestly, wheres the lie</div></li></ul>')
+}
+
+testTwoNotes()
